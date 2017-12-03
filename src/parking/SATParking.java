98c1a99db57f8e5df281c12b607183ae9a0a9c00
LuixVix <100317546@alumnos.uc3m.es>
2017-12-03 19:33:40 +0100

    Inicio del codigo de sat

--- src/parking/SATParking.java
+++ src/parking/SATParking.java
@@ -1,9 +1,52 @@
 package parking;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jacop.core.BooleanVar;
+import org.jacop.core.Store;
+import org.jacop.jasat.utils.structures.IntVec;
+import org.jacop.satwrapper.SatWrapper;
+import org.jacop.search.DepthFirstSearch;
+import org.jacop.search.IndomainMin;
+import org.jacop.search.Search;
+import org.jacop.search.SelectChoicePoint;
+import org.jacop.search.SimpleSelect;
+import org.jacop.search.SmallestDomain;
+
+import parking.coche;
+
 public class SATParking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Boolean result = false;
+		Store store = new Store();
+		SatWrapper satWrapper = new SatWrapper();
+		store.impose(satWrapper);
+		
+		
+		
+		try{
+			//Lista de variables booleanVar para el jacop
+			List<BooleanVar> listaCoches = new ArrayList<BooleanVar>();
+			//Lista de las variables coche, que contienen su categoria (de forma numerica), orden de llegada, calle en la que se encuentran y 
+			//el numero de plaza
+			List<coche> coches = new ArrayList<coche>();
+			
+			/*
+			 * 
+			 * REGLAS
+			 * 
+			 */
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
 
 	}
 
---
+++ src/parking/coche.java
@@ -0,0 +1,9 @@
+package parking;
+
+public class coche {
+	private int categoria;
+	private int llegada;
+	
+	private int calle;
+	private int n_plaza;
+}
