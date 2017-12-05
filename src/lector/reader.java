351e6d4f49ccf8166682df4b0489a35c264f0c78
LuixVix <100317546@alumnos.uc3m.es>
2017-12-05 20:29:15 +0100

    Finished reader class and created folder for parking tests

--- src/lector/reader.java
+++ src/lector/reader.java
@@ -5,44 +5,63 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class reader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		reader r = new reader();
+		Path inputFile = Paths.get(args[0]);
+		Path outputFile = Paths.get(inputFile + ".output");
+
+		try {
+			String[][] map = r.fileReader(inputFile);
+			r.closeFile(outputFile, map);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
-	
+
 	/*
-	 * Nombre: fileReader
-	 * Entrada: Path filename, contiene el nombre/direccion del archivo a
-	 * convertir
-	 * Salida: char[][], devuelve un array 2D con los valores leidos del archivo
-	 * Funcion: lee el contenido en txt del fichero que contiene el mapa y lo
-	 * parsea a un array 2D para poder moverse mejor.
+	 * Nombre: fileReader Entrada: Path filename, contiene el nombre/direccion
+	 * del archivo a convertir Salida: char[][], devuelve un array 2D con los
+	 * valores leidos del archivo Funcion: lee el contenido en txt del fichero
+	 * que contiene el mapa y lo parsea a un array 2D para poder moverse mejor.
 	 */
 	public String[][] fileReader(Path filename) throws IOException {
 		List<String> file = Files.readAllLines(filename);
-		int y = (int)(file.get(0).charAt(0));
-		int  x = (int)(file.get(0).charAt(1));
-		String[][] map = new String[x][y];
-		for (int i = 1; i < y; i++) {
-			for (int j = 0; j < 2*x; j+=2) {
-				String tmp = ""+ file.get(i).charAt(j) + file.get(i).charAt(j+1);
+		int y = file.get(0).charAt(0) - '0';
+		int x = file.get(0).charAt(1) - '0';
+		String[][] map = new String[y][x];
+		for (int i = 0; i < y; i++) {
+			int k = 0;
+			for (int j = 0; j < x; j++) {
+				String tmp = "" + file.get(i+1).charAt(k) + file.get(i+1).charAt(k + 1);
+				k += 2;
 				map[i][j] = tmp;
 			}
 		}
+		/*
+		for (int j = 0; j < y; j++) {
+			for (int j2 = 0; j2 < x; j2++) {
+				System.out.print(map[j][j2] + " ");
+			}
+			System.out.println();
+		}
+		*/
 		return map;
 	}
+
 	/*
-	 *  Nombre: closeFile
-	 *  Funcion: escribe en el fichero de destino el mapa proporcionado
-	 *  como una matriz de bytes
-	 *  
-	 *  Salida: ninguna
-	 *  
+	 * Nombre: closeFile Funcion: escribe en el fichero de destino el mapa
+	 * proporcionado como una matriz de bytes
+	 * 
+	 * Salida: ninguna
+	 * 
 	 * @param filename
+	 * 
 	 * @param map
 	 */
 	public void closeFile(Path filename, String[][] map) {
